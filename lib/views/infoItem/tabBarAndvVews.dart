0043962f17c54d6cb144d3fc0cb66b9c62cd4523
morteza roozbehi <mr.roz16126@gmail.com>
2021-12-01 13:03:25 +0330

    tab bar and tabbar view in edite 2

--- lib/views/infoItem/tabBarAndvVews.dart
+++ lib/views/infoItem/tabBarAndvVews.dart
@@ -1,3 +1,6 @@
+
+
+
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -47,17 +50,94 @@ tabBarView(wi) {
   final controller = Get.put(HomeController());
 
   return FadeInUp(
-    child: Container(
+    child:Container(
       height: wi*.9,
-      child: TabBarView(
-        controller: controller.tabController,
-        children: <Widget>[
-          Container(color: Colors.orangeAccent,height: 25,width: 25,),
-          Container(color: Colors.deepOrange,height: 25,width: 25,),
-          Container(color: Colors.pink,height: 25,width: 25,),
+      child: ListView(
+        children: [
+          Container(
+            height: wi*.9,
+            child: TabBarView(
+              controller: controller.tabController,
+              children: <Widget>[
+                infoTab(wi),
+                Container(color: Colors.deepOrange,height: 25,width: 25,),
+                Container(color: Colors.pink,height: 25,width: 25,),
 
+              ],
+            ),
+          )
         ],
       ),
+    )
+    );
+
+}
+
+
+
+infoTab(wi){
+  var tempText ='A failed reporter is bonded to an alien entity, one of many symbiotes who have invaded Earth. A failed reporter is bonded to an alien entity, one of many symbiotes who have invaded EarthBut the being takes a liking to Earth and decides to protect it.';
+
+  return Container(
+    color: Colors.orangeAccent,
+    height: wi*.05,
+    //width: 25,
+    child:ListView(
+      children: [
+        Container(
+          margin: EdgeInsets.all(wi*.02),
+          height: wi*.089,
+          width: wi*.9,
+          color: Colors.deepOrange,
+          child: Text('StoryLine',style: TextStyle(color: Colors.white,fontSize: wi*.06),),
+        ),
+        Container(
+          margin: EdgeInsets.all(wi*.02),
+          color: Colors.pink,
+          // height:more.value? geDescMore : geDesc,
+          height: wi*.3,
+          width: wi*.9,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                  tempText,style: TextStyle(color: Colors.white,fontSize: wi*.045),
+                  maxLines: 4,
+                  overflow:TextOverflow.ellipsis
+              ),
+              GestureDetector(
+                onTap: (){
+                  //     more.value =true;
+                },
+                child: Text(
+                  'More ...',style: TextStyle(color: Colors.white,fontSize: wi*.05,fontWeight: FontWeight.bold),
+
+                ),
+              ),
+
+
+            ],
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.all(wi*.02),
+          height: wi*.5,
+          width: wi*.9,
+          color: Colors.deepOrange,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+              itemCount: 4,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: EdgeInsets.symmetric(vertical: wi*.06,horizontal: wi*.02),
+                   width: wi*.4,
+                  color: Colors.blue,
+                  child: Center(child: Text('$index')),
+                );
+              },
+          ),
+        ),
+      ],
     ),
   );
 }
